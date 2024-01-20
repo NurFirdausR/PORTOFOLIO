9056f711b6947e60378321a844aa7659a26bd048
NurFirdausR <nurfirdaus.5000@gmail.com>
2024-01-20 23:59:59 +0700

    feat: add images for logos and set static data project,carrer,education,and stack skill

--- src/data/data.ts
+++ src/data/data.ts
@@ -6,128 +6,131 @@ import UniLogo from "assets/images/logos/fast.png";
 import figma from "assets/images/skills/figma.png";
 import vscode from "assets/images/skills/vscode.png";
 import react from "assets/images/skills/react.png";
-import ruby from "assets/images/skills/ruby.png";
-import rails from "assets/images/skills/rails.png";
+// import ruby from "assets/images/skills/ruby.png";
+// import rails from "assets/images/skills/rails.png";
 import javascript from "assets/images/skills/javascript.png";
 import typescript from "assets/images/skills/typescript.png";
 import html5 from "assets/images/skills/html-5.png";
 import css3 from "assets/images/skills/css3.png";
-import python from "assets/images/skills/python.png";
+// import python from "assets/images/skills/python.png";
 import bootstrap from "assets/images/skills/bootstrap.png";
 import postgres from "assets/images/skills/postgresql.png";
 import redis from "assets/images/skills/redis.png";
 import mysql from "assets/images/skills/mysql.png";
-import heroku from "assets/images/skills/heroku.png";
-import docean from "assets/images/skills/digital-ocean.png";
-import aws from "assets/images/skills/aws.png";
-import nginx from "assets/images/skills/nginx.png";
+// import heroku from "assets/images/skills/heroku.png";
+// import docean from "assets/images/skills/digital-ocean.png";
+// import aws from "assets/images/skills/aws.png";
+// import nginx from "assets/images/skills/nginx.png";
+import apache from "assets/images/skills/apache.png";
+import gcp from "assets/images/skills/gcp.png";
+import golang from "assets/images/skills/golang.png";
+import jquery from "assets/images/skills/jquery.png";
+import laravel from "assets/images/skills/laravel.png";
+import php from "assets/images/skills/php.png";
+import ubuntu from "assets/images/skills/ubuntu.png";
+import smk from "assets/images/logos/smk.jpg";
+import kuliah from "assets/images/logos/kuliah.jpeg";
+import imp from "assets/images/logos/imp.jpg";
+import ionbit from "assets/images/logos/ionbit.jpeg";
 
 export const companies = [
   {
-    title: "Phaedra Solutions",
-    alt: "PS image",
-    role: "Senior Software Engineer",
-    skills: ["ruby", "rails", "typescript", "javascript", "react", "aws"],
-    period: "2019 - Present",
-    logo: PhaedraLogo
+    title: "Informatika Media Pratama",
+    alt: "IMP image",
+    role: "Fullstack Engineer",
+    skills: ["html", "css", "javascript", "jquery", "php", "laravel","mysql","postgres"],
+    period: "May 2022 - November 2023",
+    logo: imp
   },
   {
-    title: "Brainx Technologies",
+    title: "Ionbit Indonesia",
     alt: "BT image",
-    role: "Software Engineer",
-    skills: ["ruby", "rails", "javascript", "python"],
-    period: "2018 - 2019",
-    logo: BrainxLogo
-  },
-  {
-    title: "Codenya Studio",
-    alt: "CS image",
-    role: "Co-founder, CTO",
-    skills: ["ruby", "rails", "python", "typescript", "react", "aws"],
-    period: "2018 - Present",
-    logo: CodenyaLogo
-  },
-  {
-    title: "Brainplow",
-    alt: "Brainplow image",
-    role: "Software Engineer",
-    skills: ["javascript", "python", "angular"],
-    period: "2017 - 2018",
-    logo: BrainplowLogo
+    role: "Intership",
+    skills:["html", "css", "javascript","reactjs","nextjs"],
+    period: "December 2021 - May 2022",
+    logo: ionbit
   }
+ 
 ];
 
 export const institutes = [
   {
-    short_title: "FAST - NUCES",
-    title: "National University of Computer and Emerging Sciences",
-    alt: "Nuces image",
-    role: "Bachelor's Degree in Computer Science",
-    skills: ["project management", "web development", "data structures"],
-    period: "2013 - 2017",
-    startingYear: "2013",
-    logo: UniLogo,
-    awards: [
-      {
-        title: "Speed Programming Battle",
-        description:
-          "Won speed programming battle which held at FAST - NUCES.",
-        date: "May 1, 2015"
-      },
-      {
-        title: "Silver Medal",
-        description:
-          "This medal was awarded to me by FAST - NUCES for getting a CGPA of 3.9 in 2nd semester.",
-        date: "May 1, 2014"
-      },
-      {
-        title: "Speed Programming Battle",
-        description:
-          "Won speed programming battle which held at FAST - NUCES.",
-        date: "Nov 1, 2013"
-      }
-    ]
+    short_title: "Univ Unindra",
+    title: "Universitas Indraprasta PGRI ",
+    alt: "unindra image",
+    role: "Student of Information Technology at Universitas Indraprasta PGRI",
+    skills: ["web development", "data structures"],
+    period: "2023 - now",
+    startingYear: "2023",
+    logo: kuliah
   },
   {
-    short_title: "Govt Degree College",
-    title: "Govt Degree College",
-    alt: "college image",
-    role: "FSC",
-    skills: ["mathematic", "chemistry", "physics"],
-    period: "2011 - 2013",
-    startingYear: "2011",
-    logo: ""
+    short_title: "SMK TB",
+    title: "Vocational High School",
+    alt: "tb image",
+    role: "Student",
+    skills: ["web development", "devops",],
+    period: "2019 - 2021",
+    startingYear: "2019",
+    logo: smk,
+    awards: [
+      {
+        title: "Participant PermataYouthPreneur",
+        description:
+          "Participant PermataYouthPreneur Best 45 from indonesia - Frontend - Landing Page",
+        date: "2021"
+      },
+      {
+        title: "Join Course Web Complete",
+        description:
+        "During Covid, I spent a lot of time at home, such as distance learning, etc., I used this time to take a Web Complete course.",
+        date: "2020"
+      },
+      {
+        title: "Join Course Linux Administration",
+        description:
+        "During Covid, I spent a lot of time at home, such as distance learning, etc., I used this time to take a Linux administration course.",
+        date: "2020"
+      }
+    ]
   }
 ];
 
 export const skills = [
   {
-    name: "Ruby on Rails",
+    name: "Laravel",
     description: "Web development",
-    link: "https://rubyonrails.org/",
+    link: "https://laravel.com/",
     type: "development",
-    image: rails
+    image: laravel
   },
   {
-    name: "React",
+    name: "ReactJs",
     description: "Web development",
     link: "https://reactjs.org/",
     type: "development",
     image: react
   },
   {
-    name: "Ruby",
+    name: "PHP",
     description: "Web development, scrapping",
-    link: "https://www.ruby-lang.org/en/",
+    link: "https://www.php.net/",
     type: "development",
-    image: ruby
+    image: php
   },
   {
-    name: "Python",
+    name: "Golang",
     description: "Web development, scrapping",
-    link: "https://www.python.org/",
+    link: "https://go.dev/",
     type: "development",
-    image: python
+    image: golang
+  },
+  {
+    name: "Jquery",
+    description: "Web development",
+    link: "https://jquery.com/",
+    type: "development",
+    image: jquery
   },
   {
     name: "Javascript",
@@ -186,32 +189,25 @@ export const skills = [
     image: redis
   },
   {
-    name: "Heroku",
+    name: "GCP",
     description: "Devops",
-    link: "https://www.heroku.com/",
+    link: "https://cloud.google.com/",
     type: "devops",
-    image: heroku
+    image: gcp
   },
   {
-    name: "AWS",
+    name: "Ubuntu",
     description: "Devops",
-    link: "https://aws.amazon.com/",
+    link: "https://ubuntu.com/",
     type: "devops",
-    image: aws
+    image: ubuntu
   },
   {
-    name: "Digital Ocean",
-    description: "Devops",
-    link: "https://www.digitalocean.com/",
-    type: "devops",
-    image: docean
-  },
-  {
-    name: "Nginx",
+    name: "Apache",
     description: "Web server",
-    link: "https://www.nginx.com/",
+    link: "https://httpd.apache.org/",
     type: "devops",
-    image: nginx
+    image: apache
   },
   {
     name: "Visual Studio Code",
